a5d14de81bc5b4151bbe1cebd4c2a6c5c954d5ec
leonb <leonb>
2001-12-06 23:31:49 +0000

    Reformatted. Added final exit(0).

--- gui/bin2cpp/bin2cpp.cpp
+++ gui/bin2cpp/bin2cpp.cpp
@@ -49,70 +49,73 @@
 int
 main(int argc, char ** argv)
 {
-   if (argc!=2)
-   {
+  if (argc!=2)
+    {
       fprintf(stderr, "Usage: %s <var_name>\n", argv[0]);
       exit(1);
-   }
-
-   const char * name=argv[1];
-
-      // First read all data into memory
-   GP<ByteStream> gstr_in=ByteStream::create(GURL::Filename::UTF8("-"),"rb");
-   ByteStream &str_in=*gstr_in;
-   GP<ByteStream> gmem_str1=ByteStream::create();
-   ByteStream &mem_str1=*gmem_str1;
-   mem_str1.copy(str_in);
-
-      // Now compute block size for BSByteStream
-   int block_size=1+(mem_str1.size() >> 10);
-   if (block_size<10) block_size=10;
-   if (block_size>1024) block_size=1024;
-
-      // Compress data
-   GP<ByteStream> mem_str2=ByteStream::create();
-   {
-      GP<ByteStream> bs_str=BSByteStream::create(mem_str2, block_size);
-      mem_str1.seek(0);
-      bs_str->copy(mem_str1);
-   }
-
-      // Finally: generate file
-   printf("\
-/* This is an AUTOMATICALLY generated file (by bin2cpp). STAY AWAY! */\n\
-\n\
-#include \"cin_data.h\"\n\
-\n\
-static int %s_len=%d;\n\
-static const char %s_data[]=\"\\\n", name, mem_str2->size(), name);
-
-   mem_str2->seek(0);
-   int chars=0;
-   int size=0;
-   while(1)
-   {
+    }
+  
+  const char * name=argv[1];
+  
+  // First read all data into memory
+  GP<ByteStream> gstr_in=ByteStream::create(GURL::Filename::UTF8("-"),"rb");
+  ByteStream &str_in=*gstr_in;
+  GP<ByteStream> gmem_str1=ByteStream::create();
+  ByteStream &mem_str1=*gmem_str1;
+  mem_str1.copy(str_in);
+  
+  // Now compute block size for BSByteStream
+  int block_size=1+(mem_str1.size() >> 10);
+  if (block_size<10) block_size=10;
+  if (block_size>1024) block_size=1024;
+  
+  // Compress data
+  GP<ByteStream> mem_str2=ByteStream::create();
+  {
+    GP<ByteStream> bs_str=BSByteStream::create(mem_str2, block_size);
+    mem_str1.seek(0);
+    bs_str->copy(mem_str1);
+  }
+  
+  // Finally: generate file
+  printf("/* This is an AUTOMATICALLY generated file (by bin2cpp). STAY AWAY! */\n"
+         "\n"
+         "#include \"cin_data.h\"\n"
+         "\n"
+         "static int %s_len=%d;\n"
+         "static const char %s_data[]=\"\\\n" , 
+         name, mem_str2->size(), name);
+  
+  mem_str2->seek(0);
+  int chars=0;
+  int size=0;
+  while(1)
+    {
       unsigned char ch;
       if (!mem_str2->read(&ch, 1)) break;
       printf("\\%03o", ch);
       chars+=4;
       if (chars>70)
-      {
-	 chars=0;
-	 printf("\\\n");
-      }
+        {
+          chars=0;
+          printf("\\\n");
+        }
       size++;
-   }
-
-   printf("\";\n\
-\n\
-class %s_class\n\
-{\n\
-public:\n\
-   %s_class(void)\n\
-   {\n\
-      CINData::add(\"%s\", %s_data, %s_len);\n\
-   }\n\
-};\n\
-\n\
-static %s_class %s_var;\n\n", name, name, name, name, name, name, name);
+     }
+  
+  printf("\";\n"
+         "\n"
+         "class %s_class\n"
+         "{\n"
+         "public:\n"
+         "   %s_class(void)\n"
+         "   {\n"
+         "      CINData::add(\"%s\", %s_data, %s_len);\n"
+         "   }\n"
+         "};\n"
+         "\n"
+         "static %s_class %s_var;\n\n", 
+         name, name, name, name, name, name, name);
+  exit(0);
 }
+
