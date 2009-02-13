17e52d54e7e6e195b1a34bc06dc1903e41a32608
leonb <leonb>
2009-02-13 09:46:27 +0000

    #if --> #ifdef.  Does not hurt but should not be necessary.

--- tools/ddjvu.cpp
+++ tools/ddjvu.cpp
@@ -475,13 +475,13 @@ render(ddjvu_page_t *page, int pageno)
 		     (float)((dpi*rrect.h+ih/2)/ih));
         TIFFSetField(tiff, TIFFTAG_PLANARCONFIG, PLANARCONFIG_CONTIG);
         TIFFSetField(tiff, TIFFTAG_ORIENTATION, ORIENTATION_TOPLEFT);
-# if CCITT_SUPPORT
+# ifdef CCITT_SUPPORT
         if (compression != COMPRESSION_CCITT_T6)
 # endif
-# if JPEG_SUPPORT
+# ifdef JPEG_SUPPORT
           if (compression != COMPRESSION_JPEG)
 # endif
-# if ZIP_SUPPORT
+# ifdef ZIP_SUPPORT
             if (compression != COMPRESSION_DEFLATE)
 # endif
               TIFFSetField(tiff, TIFFTAG_ROWSPERSTRIP, (uint32)64);
