86ada3e1d0132fbaaa9de6c986de0fd57507f7d8
leonb <leonb>
2009-06-08 22:50:05 +0000

    Fixed bug in bg44 syntax parsing (under win only)

--- tools/djvumake.cpp
+++ tools/djvumake.cpp
@@ -597,7 +597,10 @@ create_bg44_chunk(IFFByteStream &iff, const char *ckid, GUTF8String filespec)
     {
       if (flag_contains_bg)
         DjVuPrintErrorUTF8("%s","djvumake: Duplicate BGxx chunk\n");
-      const int i=filespec.rsearch(':');
+      int i=filespec.rsearch(':');
+      for (unsigned int j=i+1; i>0 && j<filespec.length(); j++)
+        if (filespec[j]<'0' || filespec[j]>'9')
+          i = -1;
       if (!i)
         G_THROW("djvumake: no filename specified in first BG44 specification");
       GUTF8String filename=(i<0)?filespec:GUTF8String(filespec, i);
