dd1fcc10be722c9f50ec6d62c41e0db006dfb6d2
leonb <leonb>
2009-07-30 01:49:02 +0000

    Missing semicolon in win32 declarations!

--- libdjvu/GString.h
+++ libdjvu/GString.h
@@ -132,8 +132,8 @@
 # include <inttypes.h>
 #else
 # ifdef WIN32
-typedef unsigned __int32 uint32_t
-typedef unsigned __int16 uint16_t
+typedef unsigned __int32 uint32_t;
+typedef unsigned __int16 uint16_t;
 # else
 # pragma message("Please verify defs for uint32_t and uint16_t")
 typedef unsigned int   uint32_t // verify
